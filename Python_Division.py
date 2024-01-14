d07f3e46dae8941cdfc91b00efbb6d1f856addb5
Janani Chakrapani <39048979+cjanani@users.noreply.github.com>
2024-01-14 22:51:50 +1100

    Create Python_Division.py
    Python division code

---
+++ Python_Division.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input('Enter the first number'))
+    b = int(input('Enter the second number'))
+    
+    c =  a//b; #Integer division
+    print('The output for integer division is',c);
+    d = a/b; #Float division
+    print('The output for float division is',d);
